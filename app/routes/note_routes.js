3f2d7f514ff0a79538fe5cdbcd48eaf95f277bdf
Kevin Yee <kevinyee1993@gmail.com>
2018-04-23 13:14:52 -0700

    Add initial practice data into database using post request

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -11,9 +11,14 @@ module.exports = function(app, db) {
   //could be something like json and then create a response which replies
   //to the request
   app.post('/notes', (req, res) => {
-  // You'll create your note here.
-  console.log(req.body);
-  res.send('Hello');
-});
+      const note = { text: req.body.body, title: req.body.title };
+      db.collection('notes').insert(note, (err, result) => {
+        if (err) {
+          res.send({ 'error': 'An error has occurred' });
+        } else {
+          res.send(result.ops[0]);
+        }
+      });
+    });
 
 };
