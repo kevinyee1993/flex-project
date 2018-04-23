bb27d3907c1b46939b1e9caebb7575c090638e95
Kevin Yee <kevinyee1993@gmail.com>
2018-04-23 11:27:24 -0700

    Create first post request practice

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const bodyParser     = require('body-parser');
 const app            = express();
 
 const port = 8000;
+
+//importing routes for the server to use
+//note, because we don't have db setup yet, just using an empty object
+require('./app/routes')(app, {});
+
 app.listen(port, () => {
   console.log('We are live on ' + port);
 });
