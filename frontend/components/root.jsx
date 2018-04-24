f592c11ee5072e2cfc246a9613b54cbb653f3fcb
yihwan <yihwankim@gmail.com>
2018-04-24 13:29:16 -0700

    add frontend skeleton

---
+++ frontend/components/root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { HashRouter } from 'react-router-dom';
+import App from './app';
+
+const Root = () => (
+    <HashRouter>
+      <App />
+    </HashRouter>
+);
+
+export default Root;
