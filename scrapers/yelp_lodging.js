183ae0238b5b3c8af7dc2bbceb96d0ca6dce9249
Kevin Yee <kevinyee1993@gmail.com>
2018-04-25 11:31:55 -0700

    Customize yelp scraper for specifically restaurants and lodging and add hostels in lodging search

---
+++ scrapers/yelp_lodging.js
@@ -0,0 +1,109 @@
+//SCRAPER FOR YELP, all lodgings
+//yelp scraper is dedicated to different lodgings, save these all under
+//an lodgings collection in db
+
+//can call the megafetch multiple times using a different param for each
+//and then store those
+const PostToDatabase = require('../app/util/post_request');
+
+
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+//note, pages for yelp are shown as 0, 10, 20, etc.
+//shows 10 results per page and the number above represents the first
+//result you want to start with
+//need to multiply NUM_PAGES by 10 or something
+const NUM_PAGES = 1;
+
+//add new category in json returned and use these as categories
+//landmarks, nightlife, and shopping are all pretty straightforward
+const HOTELS = "hotels";
+const HOSTELS = "hostels";
+
+const async = require('asyncawait/async');
+
+function scrape(resultNum, category) {
+  let url = `https://www.yelp.com/search?find_desc=${ category }&sortby=rating&find_loc=San+Francisco,+CA&start=`;
+
+  return fetch(`${url}${resultNum}`)
+  .then(response => response.text());
+}
+
+let allLodging = [];
+
+//ignore linter here
+async function megaScrape(category) {
+
+  //0 is 10 results 0-10
+  for(let page = 0; page < NUM_PAGES; page++) {
+
+    //0 gives 0 which gives first 10 results, 1 gives 10, 2 gives 20, etc.
+    await scrape(page * 10, category)
+    .then(body => {
+      const lodgings = [];
+      const $ = cheerio.load(body);
+
+      $('.regular-search-result').each((i, element) => {
+
+        const $element = $(element);
+        // const $name = $element.find(".rest-row-name-text");
+        const $name = $element.find(".biz-name");
+        const $numReviews = $element.find(".review-count");
+        const $address = $element.find("address");
+        const $category = $element.find(".category-str-list");
+        const $rating = $element.find(".i-stars");
+        const $price = $element.find(".business-attribute ,price-range");
+        const $image = $element.find(".photo-box-img");
+        // console.log($rating.text());
+
+        let priceToNum = $price.text().length;
+
+        const lodging = {
+          name: $name.text(),
+          numReviews: $numReviews.text(),
+          address: $address.text(),
+          tags: $category.text(),
+          rating: $rating.attr('title'),
+          // price: $price.text(),
+          price: priceToNum,
+          image: $image.attr('src'),
+        };
+
+        lodgings.push(lodging);
+      });
+
+
+    allLodging = allLodging.concat(lodgings);
+
+    });
+
+  }
+
+  //gets rid of all line breaks and extra spaces
+  for(let i = 0; i < allLodging.length; i++) {
+    Object.keys(allLodging[i]).forEach( (key) => {
+
+      if(key === "price") {
+        //do not clean up the data if we're looking at price
+      } else {
+        let value = allLodging[i][key].replace(/(?:\r\n|\r|\n)/g, ' ');
+        value = value.replace(/ +(?= )/g,'');
+        value = value.trim();
+        allLodging[i][key] = value;
+      }
+    });
+  }
+
+  console.log(allLodging);
+  // for(let i = 0; i < allLodging.length; i++) {
+    // await PostToDatabase('lodgings', allLodging[i]);
+  // }
+}
+
+
+//change this to get different categories
+// megaScrape(HOTELS);
+megaScrape(HOSTELS);
+
+//NOTE, NEED TO CHECK THE TAGS, SOME OF THE HOSTELS RETURNED ARE HOTELS
---
+++ scrapers/yelp_restaurants.js
@@ -0,0 +1,97 @@
+//SCRAPER FOR YELP, all restaurants
+const PostToDatabase = require('../app/util/post_request');
+
+
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+//note, pages for yelp are shown as 0, 10, 20, etc.
+//shows 10 results per page and the number above represents the first
+//result you want to start with
+//need to multiply NUM_PAGES by 10 or something
+const NUM_PAGES = 1;
+
+const async = require('asyncawait/async');
+
+function scrape(resultNum) {
+  let url = `https://www.yelp.com/search?find_desc=restaurants&find_loc=San+Francisco,+CA&sortby=rating&start=`;
+  return fetch(`${url}${resultNum}`)
+  .then(response => response.text());
+}
+
+let allRestaurants = [];
+
+//ignore linter here
+async function megaScrape() {
+
+  //0 is 10 results 0-10
+  for(let page = 0; page < NUM_PAGES; page++) {
+
+    //0 gives 0 which gives first 10 results, 1 gives 10, 2 gives 20, etc.
+    await scrape(page * 10)
+    .then(body => {
+      const restaurants = [];
+      const $ = cheerio.load(body);
+
+      $('.regular-search-result').each((i, element) => {
+
+        const $element = $(element);
+        // const $name = $element.find(".rest-row-name-text");
+        const $name = $element.find(".biz-name");
+        const $numReviews = $element.find(".review-count");
+        const $address = $element.find("address");
+        const $category = $element.find(".category-str-list");
+        const $rating = $element.find(".i-stars");
+        const $price = $element.find(".business-attribute ,price-range");
+        const $image = $element.find(".photo-box-img");
+        // console.log($rating.text());
+
+        //converts all $ prices to numbers so that it's easier to query
+        let priceToNum = $price.text().length;
+
+        const restaurant = {
+          name: $name.text(),
+          numReviews: $numReviews.text(),
+          address: $address.text(),
+          tags: $category.text(),
+          rating: $rating.attr('title'),
+          // price: $price.text(),
+          price: priceToNum,
+          image: $image.attr('src'),
+        };
+
+        restaurants.push(restaurant);
+      });
+
+
+    allRestaurants = allRestaurants.concat(restaurants);
+
+    });
+
+  }
+
+  //gets rid of all line breaks and extra spaces
+  for(let i = 0; i < allRestaurants.length; i++) {
+    Object.keys(allRestaurants[i]).forEach( (key) => {
+
+      if(key === "price") {
+        //do not clean up the data if we're looking at price
+      } else {
+        let value = allRestaurants[i][key].replace(/(?:\r\n|\r|\n)/g, ' ');
+        value = value.replace(/ +(?= )/g,'');
+        value = value.trim();
+        allRestaurants[i][key] = value;
+      }
+    });
+  }
+
+  //this is just to test what we're getting back
+  console.log(allRestaurants);
+
+  //uncomment all the stuff below to add to database
+  // for(let i = 0; i < allRestaurants.length; i++) {
+    // await PostToDatabase('restaurants', allRestaurants[i]);
+  // }
+}
+
+megaScrape();
