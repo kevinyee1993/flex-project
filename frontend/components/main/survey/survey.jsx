b78e79ee1dfda436d1226c08a59bf0963bb250cf
Winston Chan <winston.chan94@gmail.com>
2018-05-02 10:57:17 -0700

    Hello

--- frontend/components/main/survey/survey.jsx
+++ frontend/components/main/survey/survey.jsx
@@ -70,7 +70,7 @@ class Survey extends React.Component {
     parsedResponse["outdoors"] = preferenceOrdering["outdoors"];
     parsedResponse["culture"] = preferenceOrdering["culture"];
     parsedResponse["spa/shopping"] = preferenceOrdering["spa/shopping"];
-
+    debugger;
     this.props.history.push(`/recommendations/${Object.values(parsedResponse).join("")}`);
   }
 
@@ -104,7 +104,7 @@ class Survey extends React.Component {
     }
 
     sorted.sort(function(a, b) {
-      return a[0] - b[0];
+      return b[0] - a[0];
     });
 
     let preferenceOrdering = {
