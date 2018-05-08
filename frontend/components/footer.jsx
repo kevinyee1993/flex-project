690c5a405954896c9bfa69fc3967acd86389c5f9
yihwan <yihwankim@gmail.com>
2018-05-08 14:47:29 -0700

    update footer

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -8,9 +8,62 @@ class Header extends React.Component {
 
   render() {
     return(
-      <footer>
-        i am footer
+
+
+      <footer className="page-footer pt-4 mt-4">
+
+
+          <div className="container text-center ">
+            <div className="row title">
+              flex-project created by
+            </div>
+              <div className="row footer-links">
+
+
+                  <div className="creator col-md-2 ">
+                      <h5 className="creator-name">Kevin Yee</h5>
+                      <ul className="list-unstyled">
+                          <li>
+                              <a href="#!"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                          </li>
+                          <li>
+                              <a href="#!"><i class="fa fa-github-square" aria-hidden="true"></i></a>
+                          </li>
+                      </ul>
+                  </div>
+
+
+
+                  <div className="creator col-md-2 ">
+                      <h5 className="creator-name">Winston Chan</h5>
+                      <ul className="list-unstyled">
+                          <li>
+                              <a href="#!"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                          </li>
+                          <li>
+                              <a href="#!"><i class="fa fa-github-square" aria-hidden="true"></i></a>
+                          </li>
+                      </ul>
+                  </div>
+
+                  <div className="creator col-md-2 ">
+                      <h5 className="creator-name">Yihwan Kim</h5>
+                      <ul className="list-unstyled">
+                          <li>
+                              <a href="#!"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                          </li>
+                          <li>
+                              <a href="#!"><i class="fa fa-github-square" aria-hidden="true"></i></a>
+                          </li>
+                      </ul>
+                  </div>
+
+              </div>
+          </div>
+
+
       </footer>
+
     );
   }
 }
