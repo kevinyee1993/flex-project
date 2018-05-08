8638dd0840d634f7759cc36fd73dc5eca288b21b
yihwan <yihwankim@gmail.com>
2018-05-08 15:09:47 -0700

    fix yelp btn color

--- frontend/components/main/suggest_detail/action_sidebar.jsx
+++ frontend/components/main/suggest_detail/action_sidebar.jsx
@@ -6,11 +6,7 @@ export default ({ source }) => {
   return(
       <rb.Col xs={12} sm={4} className="action-sidebar">
         <div className="action-sidebar-container">
-        <rb.Button block>
-          <i class="fa fa-heart" aria-hidden="true"></i> &nbsp;
-            Add to Itinerary
-          </rb.Button>
-          <rb.Button href={source} target="_blank" block>
+          <rb.Button className="yelp-button" href={source} target="_blank" block>
             <i class="fa fa-yelp" aria-hidden="true"></i> &nbsp;
               Read Reviews on Yelp
             </rb.Button>
