e4643e51c5beefdcbfb54cdd26d5bb1435f070bd
yihwan <yihwankim@gmail.com>
2018-05-09 10:30:19 -0700

    fix action-sidebar

--- frontend/components/main/suggest_detail/action_sidebar.jsx
+++ frontend/components/main/suggest_detail/action_sidebar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import * as rb from 'react-bootstrap';
-
+//
+// <rb.Button className="fb-button"
+//   ><i class="fa fa-facebook" aria-hidden="true"></i></rb.Button>
 export default ({ source }) => {
 
   return(
@@ -11,9 +13,12 @@ export default ({ source }) => {
               Read Reviews on Yelp
             </rb.Button>
             <div className="social-share">
-              <rb.Button><i class="fa fa-facebook" aria-hidden="true"></i></rb.Button>
-              <rb.Button><i class="fa fa-twitter" aria-hidden="true"></i></rb.Button>
-              <rb.Button><i class="fa fa-envelope" aria-hidden="true"></i></rb.Button>
+              <rb.Button target="_blank" className="twitter-button"
+                href={"https://twitter.com/intent/tweet?text=Check%20out%20this%20cool%20activity%20in%20SF!"}>
+                <i class="fa fa-twitter" aria-hidden="true"></i></rb.Button>
+              <rb.Button target="_blank" className="mail-button"
+                href="mailto:?subject=Check out this cool activity in SF!&amp;body=Check out this site http://www.website.com.">
+                <i class="fa fa-envelope" aria-hidden="true"></i></rb.Button>
             </div>
           </div>
           </rb.Col>
